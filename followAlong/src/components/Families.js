cc4756c7e595147e4e7c55cd58bb3294007142ae
Sadia Gray <sadiagray17@gmail.com>
2023-12-04 21:19:27 -0600

    finished follow along

---
+++ followAlong/src/components/Families.js
@@ -0,0 +1,26 @@
+import { useContext } from "react";
+import { FamilyContext } from "../App";
+import React from "react";
+import "../styles.scss";
+
+
+
+const Families = () => {
+    const familyContext = useContext(FamilyContext);
+    return (<section className="header">
+        <h1>Family Trees</h1>
+        {familyContext.families.map((family) => (
+            <button
+                className={`family-button ${
+                    family.familyName === familyContext.activeFamily.familyName && "active"
+                }`}
+                key={family.familyName}
+                onClick={() => familyContext.setActiveFamily(family)}
+            >
+                {family.familyName}
+            </button>
+        ))}
+    </section>);
+};
+
+export default Families;
